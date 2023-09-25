327e3a53bdf35938afce9f9c45754f2698d2246a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-09-24 22:45:43 -0700

    Create ToggleTheme.js

---
+++ src/app/components/ToggleTheme.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import sunIcon from "/public/assets/desktop/icon-sun.svg";
+import moonIcon from "/public/assets/desktop/icon-moon.svg";
+
+export default function ToggleTheme(props) {
+  return (
+    <div className="toggle-container flex items-center">
+      <Image src={sunIcon} alt="sun icon" width="0" height="0" sizes="100vw" />
+      <div
+        className="flex items-center justify-start w-12 h-6 rounded-[32px] mr-4 ml-[15px] bg-neutral-100 box-content lg:cursor-pointer group"
+        onClick={props.toggleTheme}
+      >
+        <div
+          className={`w-[14px] h-[14px] rounded-full mx-[5px] bg-accent ${
+            props.darkMode ? "translate-x-6" : null
+          } transition-transform duration-200 ease-linear group-hover:bg-accent-light`}
+        ></div>
+      </div>
+      <Image
+        src={moonIcon}
+        alt="moon icon"
+        width="0"
+        height="0"
+        sizes="100vw"
+      />
+    </div>
+  );
+}
