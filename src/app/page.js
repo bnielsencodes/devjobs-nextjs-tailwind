e52884842bdee9560636ec1af5773370bb4af50a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-09-24 21:43:57 -0700

    Add prefers dark mode side effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
 
+  // prefers dark mode
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (prefersDark) {
+      setDarkMode(true);
+    }
+  }, []);
 
 
 }
