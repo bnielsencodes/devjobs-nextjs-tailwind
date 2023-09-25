ec81e0e67e953db2a2b26d9b6b4386af4b2683b2
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-09-24 21:49:57 -0700

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        neutral: {
+          100: "#fff",
+          200: "#f4f6f8",
+          300: "#9daec2",
+          400: "#6e8098",
+          500: "#19202d",
+          600: "#121721",
+        },
+        accent: "#5964e0",
+        "accent-light": "#939bf4",
+        placeholder: "rgb(255 255 255 0.25)",
       },
     },
   },
   plugins: [],
-}
+};
