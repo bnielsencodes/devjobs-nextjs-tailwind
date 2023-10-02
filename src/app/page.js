e9f66468d818f9032a9a7b4efc3797a9f6e1ec0c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-01 23:57:44 -0700

    Import Search component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Search from "./components/search/Search";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -29,6 +30,7 @@ export default function Home() {
           darkMode ? "bg-neutral-600" : "bg-neutral-200"
         } relative flex flex-col items-center pt-[17px] px-6 pb-[62px] md:pt-7 md:px-[39px] xl:pt-[70px] xl:px-[165px] xl:pb-[104px]`}
       >
+        <Search darkMode={darkMode} />
       </main>
     </div>
   );
