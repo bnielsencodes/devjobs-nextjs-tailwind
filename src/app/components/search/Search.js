3b9a2955acd0a0f970c0eb7a7b732fd0a346ce4a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-01 23:54:01 -0700

    Import FilterLocation component

--- src/app/components/search/Search.js
+++ src/app/components/search/Search.js
@@ -1,9 +1,12 @@
 import FilterTitle from "@/app/components/search/FilterTitle";
+import FilterLocation from "./FilterLocation";
+
 export default function Search(props) {
   return (
     <form
     >
       <FilterTitle darkMode={props.darkMode} />
+      <FilterLocation darkMode={props.darkMode} />
     </form>
   );
 }
