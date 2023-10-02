238cec7485175033c55f0d0216b095c1d770cb42
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-01 23:51:23 -0700

    Update Header styles

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -31,9 +31,9 @@ export default function Header(props) {
 
   return (
     <header
-      className={`flex items-start justify-between pt-8 h-[136px] px-6 ${
+      className={`${
         props.darkMode ? "bg-neutral-600" : "bg-neutral-200"
-      } bg-cover bg-no-repeat md:h-40 md:px-[39px] xl:px-[165px]`}
+      } flex items-start justify-between pt-8 min-h-[136px] px-6 bg-cover bg-no-repeat md:h-40 md:px-[39px] xl:px-[165px]`}
       style={{
         backgroundImage: `url(${bgImg.src})`,
       }}
