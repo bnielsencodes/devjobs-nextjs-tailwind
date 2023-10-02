848ccd0b6011aa39951259a73effaa15d46370c8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-01 23:59:08 -0700

    Add styles

--- src/app/components/MoreButton.js
+++ src/app/components/MoreButton.js
@@ -1,6 +1,11 @@
 export default function MoreButton(props) {
   return (
     <input
+      className={` ${
+        props.darkMode ? "text-neutral-100" : "text-neutral-500"
+      } w-[141px] h-12 border-0 rounded-[5px] bg-accent text-base font-bold hover:bg-accent cursor-pointer`}
+      type="button"
+      value="Load More"
     />
   );
 }
