d8734396730675ade5cf05d140b06c21bc71333c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-01 23:53:44 -0700

    Import FilterTitle component

--- src/app/components/search/Search.js
+++ src/app/components/search/Search.js
@@ -1,7 +1,9 @@
+import FilterTitle from "@/app/components/search/FilterTitle";
 export default function Search(props) {
   return (
     <form
     >
+      <FilterTitle darkMode={props.darkMode} />
     </form>
   );
 }
