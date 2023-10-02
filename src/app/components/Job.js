f60e11679c0f10ff7621187722bf2b1f279e9f6b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-01 23:58:46 -0700

    Add styles

--- src/app/components/Job.js
+++ src/app/components/Job.js
@@ -3,8 +3,13 @@ import Image from "next/image";
 export default function Job(props) {
   return (
     <div
+      className={`${
+        props.darkMode ? "bg-neutral-500" : "bg-neutral-100"
+      } relative flex flex-col items-start pt-[43px] px-[32px] pb-[35px] rounded-md mb-[49px] last-of-type:mb-0 md:mb-[54px] xl:mb-10`}
     >
       <div
+        className="absolute -top-[25px] left-8 flex items-center justify-center w-[50px] h-[50px] rounded-[14px]"
+        style={{ backgroundColor: `${props.job.brandColor}` }}
       >
         <Image
           src={props.job.logo}
@@ -21,6 +26,10 @@ export default function Job(props) {
           <p className="m-0 text-neutral-400">{props.job.contract}</p>
         </div>
         <a
+          className={`${
+            props.darkMode ? "text-neutral-100" : "text-neutral-500"
+          } block mt-[7px] mb-[9px] text-[20px] font-bold no-underline hover:text-neutral-400`}
+          href=""
         >
           {props.job.position}
         </a>
